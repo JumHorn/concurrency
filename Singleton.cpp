55b166ff64e3bb2ab42106219cf0c01e2b0bc4bb
JumHorn <JumHorn@gmail.com>
2020-11-07 20:35:54 +0800

    double check singleton

---
+++ Singleton.cpp
@@ -0,0 +1,33 @@
+#include <mutex>
+using namespace std;
+
+/*
+线程安全单例
+double check(双重加锁)
+单例创建成功后，第一个null判断可以提高效率，后面的线程不会在有锁
+多个线程同时进入第一个null判断后，需要加锁值允许一个线程创建单例
+*/
+
+class Singleton
+{
+public:
+	Singleton *getInstance()
+	{
+		if (instance == nullptr)
+		{
+			lock_guard(m_mutex);
+			if (instance == nullptr)
+				instance = new Singleton();
+		}
+		return instance;
+	}
+
+private:
+	Singleton() {}
+	~Singleton() {}
+
+	static Singleton *instance;
+	static mutex m_mutex;
+};
+
+Singleton *Singleton::instance = nullptr;
